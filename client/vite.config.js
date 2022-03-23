ee3f18e7da6f04a83003efaa276b76a2e7d7949f
Soumen Khara <soumen2015.s.k@gmail.com>
2022-03-23 18:52:12 +0530

    Initial Commit

---
+++ client/vite.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
+import dotenv from "dotenv";
+
+const dotEnvReplacement = (envSource) => {
+    const replacements = Object.entries(envSource).reduce((obj, [key, val]) => {
+        obj[`process.env.${key}`] = `"${val}"`;
+        return obj;
+    }, {});
+
+    return {
+        name: "dotenv-replacement",
+        config(obj) {
+            obj.define = obj.define || {};
+            Object.assign(obj.define, replacements);
+        },
+    };
+};
+const path = require("path");
+export default defineConfig(({ mode }) => {
+    // checking the mode is production or not
+    const env =
+        mode === "production"
+            ? dotenv.config({ path: "./.env.prod" }).parsed
+            : dotenv.config().parsed;
+    return {
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "/src"),
+                "~bootstrap": "bootstrap",
+            },
+        },
+        css: {
+            preprocessorOptions: { scss: { charset: false } },
+        },
+        plugins: [react(), eslintPlugin(), dotEnvReplacement(env)],
+    };
+});
