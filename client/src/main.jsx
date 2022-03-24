726bdafd7fc3d627bc3f169460214aa9a3f99c07
Soumen Khara <soumen2015.s.k@gmail.com>
2022-03-24 09:10:00 +0530

    feat: added store and the router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import "./scss/main.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
